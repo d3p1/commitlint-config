03c2ce733df9575b9ec76c68e5a0f21618502866
Cristian Marcelo de Picciotto <d3p1@d3p1.dev>
2023-08-29 13:42:43 +0000

    chore: add commitlint package [#1]

---
+++ commitlint.parser.js
@@ -0,0 +1,12 @@
+/**
+ * @description Configuration for commitlint parser presets
+ * @author      Cristian Marcelo de Picciotto <d3p1@d3p1.dev>
+ * @note        Add ticket information
+ * {@link       https://commitlint.js.org/#/reference-configuration?id=parser-presets}
+ */
+module.exports = {
+    parserOpts: {
+        headerPattern       : /^(\w+)\(?(\w*)\)?:\s(.+)\s\[(.+)\]$/,
+        headerCorrespondence: ['type', 'scope', 'subject', 'ticket'],
+    }
+};
