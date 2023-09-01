5008725f1719dfac81a233c9b5a14aa7ddc47774
Cristian Marcelo de Picciotto <d3p1@d3p1.dev>
2023-09-01 18:17:19 +0000

    chore: add prettier and eslint configuration files [#1]

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+/**
+ * @description Configuration for `eslint`
+ * @author      Cristian Marcelo de Picciotto <d3p1@d3p1.dev>
+ * {@link       https://eslint.org/}
+ * {@link       https://eslint.org/docs/latest/use/configure/}
+ */
+module.exports = {
+  "extends": ["google", "prettier"]
+}
---
+++ prettier.config.js
@@ -0,0 +1,21 @@
+/**
+ * @description Configuration for `prettier`
+ * @author      Cristian Marcelo de Picciotto <d3p1@d3p1.dev>
+ * {@link       https://prettier.io/}
+ * {@link       https://prettier.io/docs/en/options}
+ */
+module.exports = {
+  "printWidth"               : 80,
+  "tabWidth"                 : 2,
+  "useTabs"                  : false,
+  "semi"                     : true,
+  "singleQuote"              : true,
+  "quoteProps"               : "consistent",
+  "trailingComma"            : "none",
+  "bracketSpacing"           : false,
+  "bracketSameLine"          : true,
+  "arrowParens"              : "always",
+  "proseWrap"                : "preserve",
+  "htmlWhitespaceSensitivity": "css",
+  "endOfLine"                : "lf"
+}
